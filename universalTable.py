1a04e33c87d97694d89c9b2015948c19537dedc5
ACG <anandaguneratne@gmail.com>
2015-09-16 13:49:42 -0500

    #5 - More work on python interface

--- universalTable.py
+++ universalTable.py
@@ -18,7 +18,7 @@ def addResource():
     description = raw_input("Description: ")
     typeID = raw_input("ID of resource type: ")
     parent = raw_input("Enter parent ID (if applicable): ")
-    userID = getUserId(usr)
+    userID = getIDFromName(usr)
     updateDate = datetime.datetime.now()
     try:
         int(typeID)
@@ -46,12 +46,63 @@ def addResource():
             parent)
         )
 
-def getUserId(username):
-    return "1"
+def getIDFromName(name):
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users WHERE name = \"{}\"".format(name))
+    result = cursor.fetchall()
+    if result: return result[0]
+    return "0"
+
+def addUser():
+    global usr
+    fName = raw_input("First name: ")
+    mName = raw_input("Middle name: ")
+    lName = raw_input("Last name: ")
+    role_id = raw_input("Role ID: ")
+    email = raw_input("Email: ")
+    phone = raw_input("Phone: ")
+    lastUser = getIDFromName(usr)
+    updateDate = datetime.datetime.now()
+    try:
+        int(role_id)
+    except:
+        print "Error! invalid data dype used"
+        return
+    cursor = db.cursor()
+    cursor.execute(
+        """
+        INSERT INTO users
+            (first_name,
+            mid_name,
+            last_name,
+            role_id,
+            mail,
+            phone,
+            last_updated_by,
+            last_updated_date
+            VALUES (\"{}\",\"{}\",\"{}\",{},\"{}\",\"{}\",{},{})
+        """.format(
+            fName,
+            mName,
+            lName,
+            role_id,
+            email,
+            phone,
+            lastUser,
+            updateDate)
+        )
     
 while True:
-    addResource()
-
+    choice = raw_input("\nChoose:" +
+                       "\n0) Commit changes" +
+                       "\n1) Add resource" +
+                       "\n2) Add user" +
+                       "\n")
+    if choice == "0": db.commit()
+    elif choice == "1": addResource()
+    elif choice == "2": addUser()
+    print "\n"
+"""
     # create a cursor
     cursor = db.cursor()
 
@@ -66,4 +117,4 @@ while True:
         print record
         #print record[0] , "–>", record[1]
     db.commit()
-    raw_input("")
+"""
