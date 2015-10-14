096027febd0cbda35be7799aa7872f8255a0fd01
adhan06 <hanson.alex@yahoo.com>
2015-10-13 21:05:17 -0500

    Added template for signup/signin
    Started working on the sign up and in with working links. Still need to
    connect to database from here.

---
+++ schedular.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+	return render_template("index.html")	
+	
+@app.route("/signup/")
+def sign_up():
+	return render_template("signup.html")
+	
+if __name__ == "__main__":
+	app.run()
