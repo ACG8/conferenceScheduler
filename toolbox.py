343988dea9ef326006a6f2ab2da74a2e05f8e502
ACG <anandaguneratne@gmail.com>
2015-10-14 01:03:14 -0500

    #8 - Packaged functions as an object
    SQL-python interface functions are now part of the class
    "Connection" and work as part of a connection object.

--- toolbox.py
+++ toolbox.py
@@ -6,6 +6,79 @@ import MySQLdb
 import getpass
 import datetime
 
+class Connection:
+
+    def __init__(self,user,password,database):
+        self.user = user
+        self.db = MySQLdb.connect(host="localhost", user=user, passwd=password, db=database)
+
+    def getAttributes(self,table):
+        "Returns the fields of the table and their associated datatypes"
+        cursor = self.db.cursor()
+        cursor.execute("SHOW COLUMNS FROM {}".format(table))
+        columns = cursor.fetchall()
+        columns = [c[:2] for c in columns]
+        return columns
+
+    def getTables(self):
+        "Returns a list of tables in the database"
+        cursor = self.db.cursor()
+        cursor.execute("SHOW TABLES")
+        tables = cursor.fetchall()
+        return tables
+
+    def update(self,table,CAs,CVs,FAs,FOs,FVs):
+        "Changes each attribute in CAs to the value in CVs when filter FAs,FOs,FVs holds"
+        
+        whereStatement = getFilter(FAs,FOs,FVs)
+        #you cannot update without a where statement or a matching set of change attributes and values
+        if not (whereStatement): return "ERROR - FAILED TO PARSE FILTERS"
+        if not (len(CAs)==len(CVs)): return "ERROR - ATTRIBUTE AND VALUE LIST LENGTHS DO NOT MATCH"
+        for i,v in enumerate(CVs):
+            try: CVs[i] = str(int(v))                        #Integers
+            except ValueError: CVs[i] = "\"{}\"".format(v)   #Strings
+        setStatement = ", ".join(["{} = {}".format(CAs[i],v) for i,v in enumerate(CVs)])
+        query = "UPDATE {} SET {} WHERE {}".format(table,setStatement,whereStatement)
+        cursor = self.db.cursor()
+        cursor.execute(query)
+        return "SUCCESS; {} UPDATED TO SET {} WHERE {}".format(table,setStatement,whereStatement)
+
+    def select(self,table,SAs,FAs,FOs,FVs):
+        "Returns the values of the requested SAs given filters FAs,FOs,FVs"
+        whereStatement = getFilter(FAs,FOs,FVs)
+        #you cannot update without a where statement or a matching set of change attributes and values
+        if not (whereStatement): return "ERROR - FAILED TO PARSE FILTERS"
+        selectStatement = ", ".join(SAs)
+        if not selectStatement: selectStatement = "*"
+        query = "SELECT {} FROM {} WHERE {}".format(selectStatement,table,whereStatement)
+        cursor = self.db.cursor()
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+
+    def selectAll(self,table,SAs):
+        "Returns the entire table, with just attributes SAs"
+        selectStatement = ", ".join(SAs)
+        if not selectStatement: selectStatement = "*"
+        cursor = self.db.cursor()
+        cursor.execute("SELECT {} FROM {}".format(selectStatement,table))
+        result = cursor.fetchall()
+        return result
+
+    def append(self,table,tuples,fields=None):
+        "Appends given tuples to table"
+        fieldStatement = ""
+        if fields: fieldStatement = "({}) ".format(", ".join(fields))
+        valuesStatement = ", ".join(tuples)
+        statement = "INSERT INTO {} {}VALUES {};".format(table,fieldStatement,valuesStatement)
+        print statement
+        cursor  = self.db.cursor()
+        cursor.execute(statement)
+
+    def commit(self):
+        self.db.commit()
+
+
 def sanitize(statement):
     "Sanitizes a statement by removing quotes and semicolns to prevent SQL injection attacks"
     return statement.replace(";","").replace("'","").replace("\"","")
@@ -18,26 +91,6 @@ def connect():
     db = MySQLdb.connect(host="localhost", user=usr, passwd=passd, db=database)
     return db
 """
-
-def connect(usr,passd,database):
-    "Connect to the given database"
-    db = MySQLdb.connect(host="localhost", user=usr, passwd=passd, db=database)
-    return db
-
-def getAttributes(db,table):
-    "Returns the fields of the table and their associated datatypes"
-    cursor = db.cursor()
-    cursor.execute("SHOW COLUMNS FROM {}".format(table))
-    columns = cursor.fetchall()
-    columns = [c[:2] for c in columns]
-    return columns
-
-def getTables(db):
-    "Returns a list of tables in the database"
-    cursor = db.cursor()
-    cursor.execute("SHOW TABLES")
-    tables = cursor.fetchall()
-    return tables
 
 def getFilter(attributes,operators,values):
     "Returns a properly formatted where clause"
@@ -52,58 +105,7 @@ def getFilter(attributes,operators,values):
     output = ", ".join(["{} {} {}".format(attributes[i],operators[i],v) for i,v in enumerate(values)])
     return output
 
-def update(db,table,CAs,CVs,FAs,FOs,FVs):
-    "Changes each attribute in CAs to the value in CVs when filter FAs,FOs,FVs holds"
-    cursor = db.cursor()
-    whereStatement = getFilter(FAs,FOs,FVs)
-    #you cannot update without a where statement or a matching set of change attributes and values
-    if not (whereStatement): return "ERROR - FAILED TO PARSE FILTERS"
-    if not (len(CAs)==len(CVs)): return "ERROR - ATTRIBUTE AND VALUE LIST LENGTHS DO NOT MATCH"
-    for i,v in enumerate(CVs):
-        try: CVs[i] = str(int(v))                        #Integers
-        except ValueError: CVs[i] = "\"{}\"".format(v)   #Strings
-    setStatement = ", ".join(["{} = {}".format(CAs[i],v) for i,v in enumerate(CVs)])
-    query = "UPDATE {} SET {} WHERE {}".format(table,setStatement,whereStatement)
-    cursor.execute(query)
-    return "SUCCESS; {} UPDATED TO SET {} WHERE {}".format(table,setStatement,whereStatement)
-
-def select(db,table,SAs,FAs,FOs,FVs):
-    "Returns the values of the requested SAs given filters FAs,FOs,FVs"
-    whereStatement = getFilter(FAs,FOs,FVs)
-    #you cannot update without a where statement or a matching set of change attributes and values
-    if not (whereStatement): return "ERROR - FAILED TO PARSE FILTERS"
-    selectStatement = ", ".join(SAs)
-    if not selectStatement: selectStatement = "*"
-    query = "SELECT {} FROM {} WHERE {}".format(selectStatement,table,whereStatement)
-    cursor = db.cursor()
-    cursor.execute(query)
-    result = cursor.fetchall()
-    return result
-
-def selectAll(db,table,SAs):
-    "Returns the entire table, with just attributes SAs"
-    selectStatement = ", ".join(SAs)
-    if not selectStatement: selectStatement = "*"
-    cursor = db.cursor()
-    cursor.execute("SELECT {} FROM {}".format(selectStatement,table))
-    result = cursor.fetchall()
-    return result
-
-def append(db,table,tuples,fields=None):
-    "Appends given tuples to table"
-    fieldStatement = ""
-    if fields: fieldStatement = "({}) ".format(", ".join(fields))
-    valuesStatement = ", ".join(tuples)
-    statement = "INSERT INTO {} {}VALUES {};".format(table,fieldStatement,valuesStatement)
-    print statement
-    cursor  = db.cursor()
-    cursor.execute(statement)
-
 #####To do: package everything into a class, db as property of class
-
-
-def commit(db):
-    db.commit()
 
 def listCommands():
     "Lists all commands (and their arguments) in this module"
@@ -118,34 +120,6 @@ def listCommands():
     print "append(db,table,tuples,fields=None)"
     print "commit(db)"
     print "listCommands()"
-    
 
-"""
-def append(db, table):
-    "Appends datum to table (string)"
-    cursor  = db.cursor()
-    cursor.execute("SHOW COLUMNS FROM {}".format(table))
-    columns = cursor.fetchall()
-    keyList = []
-    valueList = []
-    append0 = keyList.append
-    append1 = valueList.append
-    for column in columns:
-        #print column
-        if not ("auto_increment") in column:
-            key = column[0]
-            append0(column[0])
-            if "datetime" in column: append1("'{}'".format(str(datetime.datetime.now())))
-            else:
-                value = raw_input('{}: '.format(key))
-                if not value: value = "NULL"
-                append1(value)
-    keys = ",".join(keyList)
-    values = ",".join(valueList)
-    statement = "INSERT INTO {} ({}) VALUES ({});".format(table,keys,values)
-    print statement
-    cursor.execute(statement)
-"""
-
-print "Connection automatically established with conferencescheduler as 'db' for debugging purposes"
-db = connect("ananda","password","conferencescheduler")
+print "Connection automatically established with conferencescheduler connectionas 'db' for debugging purposes"
+db = Connection("ananda","password","conferencescheduler")
