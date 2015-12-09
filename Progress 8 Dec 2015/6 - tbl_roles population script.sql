6e8ddfedc92d803c67dc28a048c891c8c9ab7fbb
ACG <anandaguneratne@gmail.com>
2015-12-08 22:13:45 -0600

    More updates to progress report

---
+++ Progress 8 Dec 2015/6 - tbl_roles population script.sql
@@ -0,0 +1,6 @@
+use scheduler;
+
+insert into tbl_roles (id,name, description, privilege) values (1,'New', '', '');
+insert into tbl_roles (id,name, description, privilege) values (2,'User', '', '');
+insert into tbl_roles (id,name, description, privilege) values (3,'Manager', '', '');
+insert into tbl_roles (id,name, description, privilege) values (4,'Administrator', '', '');
